e59746ea77fcb5fd8cbd30d89291a1b8b48496ed
JPVelandia <46972093+JPVelandia@users.noreply.github.com>
2019-01-25 00:57:23 -0500

    Add files via upload

---
+++ Triángulo 3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z = 3;
+            double c = 56.31;
+            double c1 = c * (Math.PI / 180);
+
+            double t = z / Math.Sin(c1);
+            //*double t1 = t * (180 / Math.PI);/*
+
+            double y = Math.Sqrt((t * t) - (z * z));
+
+            double a = 180 - c - 90;
+
+
+            Console.WriteLine("La hipotenusa mide {0}",t);
+
+            Console.WriteLine("El cateto y mide {0}",y);
+
+            Console.WriteLine("El ánulo a mide {0}",a);
+
+        }
+    }
+}
