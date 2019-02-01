927007bc26b0b0f4e628d1fd7a1c53d6a0012866
JPVelandia <46972093+JPVelandia@users.noreply.github.com>
2019-02-01 13:27:55 -0500

    Add files via upload

---
+++ Validación.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eres un robot?");
+            Console.WriteLine("Resolver 9 + 4");
+            int respuesta = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            while (respuesta != 13)
+            {
+                
+                contador++;
+
+                Console.WriteLine("Intente otra vez");
+                Console.WriteLine("Resolver 9 + 4");
+                respuesta = int.Parse(Console.ReadLine());
+
+
+                if (contador == 2)
+                {
+                    Console.WriteLine("Ya no tiene más intentos");
+                    break;
+
+                }
+            }
+
+            Console.WriteLine("Eres humano");
+
+
+        }
+    }
+}
