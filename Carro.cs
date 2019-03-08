3a8ffa13002951d60870a521dd625cba39bd3963
JPVelandia <46972093+JPVelandia@users.noreply.github.com>
2019-03-08 14:03:00 -0500

    Add files via upload

--- Carro.cs
+++ Carro.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Carro{
+public class Carro
+{
 
     /* ---------------------------------------------------------------
      * Atributos:
@@ -12,63 +13,144 @@ public class Carro{
     public Carroceria carroceria;
     public Llanta[] llantas;
     public Puerta[] puertas;
+    public Luces[] luces;
 
 
     /* ---------------------------------------------------------------
      * Constructor:
      * En este espacio se inicializan los atributos
      * --------------------------------------------------------------- */
-    public Carro(Vector3 _posicion) {
+    public Carro(Vector3 _posicion)
+    {
 
         materializador = new Materializador(_posicion);
         carroceria = new Carroceria(materializador);
 
         llantas = new Llanta[4];
         puertas = new Puerta[4];
+        luces = new Luces[4];
 
         for (int i = 0; i < llantas.Length; i++)
         {
             llantas[i] = new Llanta(materializador, i);
             puertas[i] = new Puerta(materializador, i);
+            luces[i] = new Luces(materializador, i);
         }
 
-   
     }
 
-    public void Pintar(Color color)
+    public Carro(Vector3 _posicion, Color _color)
     {
-        carroceria.ColorPrincipal = color;
 
-        for(int i = 0; i < llantas.Length; i++)
-        {
-            puertas[i].ColorPrincipal = color;
-        }
-    }
+        materializador = new Materializador(_posicion);
+        carroceria = new Carroceria(materializador);
 
-
-    public void AbrirPuertas()
-    {
+        llantas = new Llanta[4];
+        puertas = new Puerta[4];
+        luces = new Luces[4];
 
         for (int i = 0; i < llantas.Length; i++)
         {
-            puertas[i].Abrir();
+            llantas[i] = new Llanta(materializador, i);
+            puertas[i] = new Puerta(materializador, i);
+            luces[i] = new Luces(materializador, i);
         }
+
+
+        Pintar(_color);
     }
 
-    public void CerrarPuertas()
+
+    public Carro(Vector3 _posicion, Color _color,Color _color1)
     {
+
+        materializador = new Materializador(_posicion);
+        carroceria = new Carroceria(materializador);
+
+        llantas = new Llanta[4];
+        puertas = new Puerta[4];
+        luces = new Luces[4];
 
         for (int i = 0; i < llantas.Length; i++)
         {
-            if (Input.GetKeyDown("space"))
-            {
-                puertas[i].Cerrar();
-            }
-            
+            llantas[i] = new Llanta(materializador, i);
+            puertas[i] = new Puerta(materializador, i);
+            luces[i] = new Luces(materializador, i);
+        }
+
+
+        Pintar(_color,_color1);
+        
+    }
+
+    
+
+
+    public void Pintar(Color color1)
+    {
+        carroceria.ColorPrincipal = color1;
+
+        for (int i = 0; i < puertas.Length;i++)
+        {
+            puertas[i].ColorPrincipal = color1;
         }
     }
 
+    public Carro(Vector3 _posicion,float[] llantass)
+    {
+        _posicion.y = 0;
 
+        materializador = new Materializador(_posicion);
+        carroceria = new Carroceria(materializador);
+
+        llantas = new Llanta[4];
+        puertas = new Puerta[4];
+        luces = new Luces[4];
+
+        for (int i = 0; i < llantas.Length; i++)
+        {
+            llantas[i] = new Llanta(materializador, i);
+            puertas[i] = new Puerta(materializador, i);
+            luces[i] = new Luces(materializador, i);
+        }
+
+        for (int i=0;i < llantas.Length;i++)
+        {
+            llantas[i].Radio = llantass[i];
+        }
+
+
+
+    }
+
+
+
+
+
+
+    public void Pintar(Color color2, Color color1)
+    {
+        carroceria.ColorPrincipal = color2;
+            puertas[0].ColorPrincipal = color1;
+            puertas[1].ColorPrincipal = color1;
+            puertas[2].ColorPrincipal = color2;
+            puertas[3].ColorPrincipal = color2;
+        
+    }
+
+    public void Pintar()
+    {
+
+        float a = Random.Range(0,1);
+        Color c = new Color(a,a,a);
+        carroceria.ColorPrincipal = c;
+        for (int i = 0; i < puertas.Length; i++)
+        {
+            float b = Random.Range(0, 1);
+            Color c1 = new Color(b, b, b);
+            puertas[i].ColorPrincipal = c;
+        } 
+    }
 
 
 }
--- Program.cs
+++ Program.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class Program {
 
-    public static void Main (string[] args) {
+    public static void Main(string[] args) {
 
-        Vector3 vector = new Vector3(0,0,1);
-        Carro carro = new Carro(vector);
+        Color color1 = new Color(0.3f, 0.1f, 0.2f);
+        Color color2 = new Color(0.01f, 0.3f, 1f);
 
-        Color azul = Color.blue;
-        carro.Pintar(azul);
+        Vector3 pos = new Vector3(2, 2, 0);
+        Vector3 pos1 = new Vector3(4, 4, 0);
+        Vector3 pos2 = new Vector3(6, 6, 0);
+        Vector3 pos3 = new Vector3(8, 8, 0);
 
-        carro.AbrirPuertas();
+        float[] radiocarros = {0.1f,0.4f,0.5f,0.5f };
 
-        carro.CerrarPuertas();
+
+
+        Carro carro1 = new Carro(pos,color1,color2);
+        Carro carro2 = new Carro(pos1,color1);
+        Carro carro3 = new Carro(pos2,radiocarros);
+        Carro carro4 = new Carro(pos3);
+        carro4.Pintar();
 
     }
-
-
-   
-
 
 
 }
